fc713e44f1285bd21ab1c9944d67405c69e87ec8
Soslan Aldatov <me@sptm.dev>
2021-03-30 17:43:23 +0500

    WIP: add game state interfaces

--- mafia-server/src/game/interfaces/game.interface.ts
+++ mafia-server/src/game/interfaces/game.interface.ts
@@ -1,11 +1,27 @@
 import { IPlayer } from './player.interface';
 
 export type Team = 'mafia' | 'civilians' | undefined;
+export type GamePhase = 'day' | 'night' | 'voting' | 'created' | 'finished';
 
+export class IVote {
+  player: IPlayer;
+  vote: IPlayer;
+}
+
+export class IShooting {
+  player: IPlayer;
+  shot: IPlayer;
+}
+export class IGameState {
+  phase: GamePhase;
+  lastVote: IVote[];
+  lastShooting: IShooting[];
+}
 export class IGame {
   janusSession: string | undefined;
   videoRoom: string | undefined;
   day: number;
   winner: Team;
   players: IPlayer[];
+  gameState: IGameState;
 }
