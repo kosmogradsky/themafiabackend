488985d3a9c6fc19b7747c87e0896d3266f844e9
Soslan Aldatov <me@sptm.dev>
2021-03-30 17:43:23 +0500

    WIP: add game state interfaces

--- mafia-server/src/game/interfaces/player.interface.ts
+++ mafia-server/src/game/interfaces/player.interface.ts
@@ -4,6 +4,7 @@ export type Role = MafiaRole | CivilianRole | 'narrator' | undefined;
 
 export class IPlayer {
   //TODO: link with User once it's ready
+  id: number;
   name: string;
   is_alive: boolean;
   is_exposed: boolean;
