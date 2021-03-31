32ca7f0d8a32c454e0fdbf6f56e27c98d18ef5b0
Soslan Aldatov <me@sptm.dev>
2021-03-31 15:29:17 +0500

    import new entities to game module

--- mafia-server/src/game/game.module.ts
+++ mafia-server/src/game/game.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from './entities/game.entity';
+import { GameState } from './entities/gamestate.entity';
 import { Player } from './entities/player.entity';
+import { Shot } from './entities/shot.entity';
+import { Vote } from './entities/vote.entity';
 import { GameService } from './services/game.service';
 import { PlayerService } from './services/player.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Player, Game])],
+  imports: [TypeOrmModule.forFeature([Player, Game, GameState, Vote, Shot])],
   exports: [TypeOrmModule],
   providers: [PlayerService, GameService],
 })
