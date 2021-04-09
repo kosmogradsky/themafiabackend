c6bbf75d06dc1b51d98f389177db0a0598b21806
Soslan Aldatov <me@sptm.dev>
2021-04-09 13:28:38 +0500

    add ID to interfaces

--- mafia-server/src/game/interfaces/game.interface.ts
+++ mafia-server/src/game/interfaces/game.interface.ts
@@ -4,22 +4,26 @@ export type Team = 'mafia' | 'civilians' | undefined;
 export type GamePhase = 'day' | 'night' | 'voting' | 'created' | 'finished';
 
 export class IVote {
+  id: number;
   player: IPlayer;
   choice: IPlayer;
   readonly created_at?: string;
 }
 
 export class IShot {
+  id: number;
   player: IPlayer;
   aim: IPlayer;
   readonly created_at?: string;
 }
 export class IGameState {
+  id: number;
   phase: GamePhase;
   lastVote: IVote[];
   lastShooting: IShot[];
 }
 export class IGame {
+  id: number;
   janusSession: string | undefined;
   videoRoom: string | undefined;
   day: number;
