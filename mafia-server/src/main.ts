869169bc2f5e87dfa3039a53f993ac85c02a839a
Soslan Aldatov <me@sptm.dev>
2021-04-12 14:31:41 +0500

    add Redis adapter and stuff

--- mafia-server/src/main.ts
+++ mafia-server/src/main.ts
@@ -3,9 +3,11 @@ ConfigModule.forRoot({ isGlobal: true });
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './core/adapters/redisio.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
