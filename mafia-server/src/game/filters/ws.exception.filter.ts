9d3b0940c32ff4ff7430419083e36ed1b1ddac9b
Soslan Aldatov <me@sptm.dev>
2021-04-19 18:11:00 +0500

    add socket.io error logging/exception filter

--- mafia-server/src/game/filters/ws.exception.filter.ts
+++ mafia-server/src/game/filters/ws.exception.filter.ts
@@ -1,11 +1,21 @@
-import { ArgumentsHost, Catch } from '@nestjs/common';
-import { BaseWsExceptionFilter } from '@nestjs/websockets';
-
-//TODO: actually implement filters
+import { ArgumentsHost, Catch, Logger } from '@nestjs/common';
+import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
 
 @Catch()
 export class GameWsExceptionFilter extends BaseWsExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
+  private logger: Logger = new Logger(GameWsExceptionFilter.name);
+
+  catch(exception: WsException, host: ArgumentsHost) {
+    const ctx = host.switchToWs();
+    const data = ctx.getData();
+    const client = ctx.getClient();
+
+    const error = exception.getError();
+
+    this.logger.log(
+      `Handling WS error ${error.toString}: request from client ${client} with data ${data}`,
+    );
+
     super.catch(exception, host);
   }
 }
