6229ef18b49aad2e94a9b2bad941e633d1fb5bc4
Soslan Aldatov <me@sptm.dev>
2021-04-21 10:32:00 +0500

    add media requests DTOs for input valudation

---
+++ mafia-server/src/game/dto/janus.request.dto.ts
@@ -0,0 +1,43 @@
+export type JanusCommand =
+  | 'create'
+  | 'destroy'
+  | 'attach'
+  | 'detach'
+  | 'message'
+  | 'hangup';
+export type JanusPlugin =
+  | 'videocall'
+  | 'videoroom'
+  | 'nosip'
+  | 'textroom'
+  | 'echotest';
+
+export class JanusCommandBody {
+  request?:
+    | 'create'
+    | 'destroy'
+    | 'edit'
+    | 'exists'
+    | 'list'
+    | 'allowed'
+    | 'kick'
+    | 'listparticipants';
+  room?: number;
+  is_private?: boolean;
+  allowed?: string[];
+  id?: string;
+  audio?: boolean;
+  video?: boolean;
+}
+
+export class JanusRequestDTO {
+  janus: JanusCommand;
+  body?: JanusCommandBody;
+  plugin?: JanusPlugin;
+  transaction?: string;
+  apisecret?: string;
+}
+
+export class JanusCommandRequestDTO extends JanusRequestDTO {
+  janus: 'message';
+}
