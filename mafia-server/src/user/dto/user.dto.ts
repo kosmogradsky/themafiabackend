e1d33f37804b780bbabfb7416114b41e91296ab7
Soslan Aldatov <me@sptm.dev>
2021-04-26 12:59:27 +0500

    update User DTO

--- mafia-server/src/user/dto/user.dto.ts
+++ mafia-server/src/user/dto/user.dto.ts
@@ -1,9 +1,19 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 import { IPlayer } from 'src/game/interfaces/player.interface';
 
 export class UserDTO {
+  @IsNotEmpty()
   readonly id?: number;
+
+  @IsNotEmpty()
   username?: string;
+
+  @IsNotEmpty()
   password?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email?: string;
+
   players?: IPlayer[];
 }
