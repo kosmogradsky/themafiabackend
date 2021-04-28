6cf49ec3f4850b66ccfa6eed6a18cfc2190a0016
Soslan Aldatov <me@sptm.dev>
2021-04-28 10:49:48 +0500

    add auth service

---
+++ mafia-server/src/auth/services/auth.service.ts
@@ -0,0 +1,62 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserDTO } from 'src/user/dto/user.dto';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/services/user.service';
+import { IJwtPayload } from '../interfaces/jwt.payload.interface';
+import { IJwtToken, ILoginStatus } from '../interfaces/login.status.interface';
+import { IRegistrationStatus } from '../interfaces/registration.status.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private _createToken({ username }: UserDTO): IJwtToken {
+    const user: IJwtPayload = { username };
+    const accessToken = this.jwtService.sign(user);
+    return {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+      accessToken,
+    };
+  }
+
+  async register(userDTO: UserDTO): Promise<IRegistrationStatus> {
+    let status: IRegistrationStatus = {
+      success: true,
+      message: 'Registration successful',
+    };
+
+    try {
+      await this.userService.create(userDTO);
+    } catch (err) {
+      status = {
+        success: false,
+        message: err,
+      };
+    }
+
+    return status;
+  }
+
+  async login(userDTO: UserDTO): Promise<ILoginStatus> {
+    const user = await this.userService.login(userDTO);
+    const token = this._createToken(user);
+
+    return {
+      username: user.username,
+      ...token,
+    };
+  }
+
+  async validateUser(payload: IJwtPayload): Promise<User> {
+    const user = await this.userService.getByUsername(payload);
+    if (!user) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
+  }
+}
