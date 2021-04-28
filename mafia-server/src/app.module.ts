6042d5257e900bf224fd31fbfdb76c801380784f
Soslan Aldatov <me@sptm.dev>
2021-04-28 10:45:42 +0500

    add auth module

--- mafia-server/src/app.module.ts
+++ mafia-server/src/app.module.ts
@@ -7,9 +7,10 @@ import { DatabaseModule } from './core/database/database.module';
 import { GameModule } from './game/game.module';
 import { ApiModule } from './api/api.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, GameModule, ApiModule, UserModule],
+  imports: [DatabaseModule, GameModule, ApiModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ mafia-server/src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [PassportModule, JwtModule],
+})
+export class AuthModule {}
