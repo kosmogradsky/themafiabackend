36839a34061f70030d29ac6bd115cc220f8f7290
Soslan Aldatov <me@sptm.dev>
2021-04-29 13:09:42 +0500

    WIP: add Swagger stuff

--- mafia-server/src/main.ts
+++ mafia-server/src/main.ts
@@ -2,12 +2,22 @@ import { ConfigModule } from '@nestjs/config';
 ConfigModule.forRoot({ isGlobal: true });
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './core/adapters/redisio.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new RedisIoAdapter(app));
+
+  const config = new DocumentBuilder()
+    .setTitle('Mafia Backend')
+    .setDescription('Mafia backend API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
