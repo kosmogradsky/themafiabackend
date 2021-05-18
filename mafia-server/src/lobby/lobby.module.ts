1e756c875669d56f044b46f894dc94a5f895f94b
Soslan Aldatov <me@sptm.dev>
2021-05-18 22:17:25 +0500

    add typeorm stuff to lobby module

--- mafia-server/src/lobby/lobby.module.ts
+++ mafia-server/src/lobby/lobby.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LobbyService } from './lobby.service';
 import { LobbyController } from './lobby.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lobby } from './entities/lobby.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Lobby])],
   controllers: [LobbyController],
   providers: [LobbyService],
 })
