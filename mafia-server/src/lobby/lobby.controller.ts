142a08acdf4880e2ac12d4602cd92ca0d13dfa13
Soslan Aldatov <me@sptm.dev>
2021-05-18 22:13:51 +0500

    Lobby API controller / Swagger stuff

--- mafia-server/src/lobby/lobby.controller.ts
+++ mafia-server/src/lobby/lobby.controller.ts
@@ -6,37 +6,94 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { LobbyService } from './lobby.service';
 import { CreateLobbyDto } from './dto/create-lobby.dto';
 import { UpdateLobbyDto } from './dto/update-lobby.dto';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ILobby } from './interfaces/lobby.interface';
 
+@ApiTags('lobby')
 @Controller('lobby')
 export class LobbyController {
   constructor(private readonly lobbyService: LobbyService) {}
 
+  @ApiOperation({ summary: 'Lobby creation' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiOkResponse({
+    status: 201,
+    description: 'Lobby created successfully',
+    type: ILobby,
+  })
   @Post()
-  create(@Body() createLobbyDto: CreateLobbyDto) {
-    return this.lobbyService.create(createLobbyDto);
+  async create(@Body() createLobbyDto: CreateLobbyDto) {
+    const lobby: ILobby = await this.lobbyService.create(createLobbyDto);
+    if (!lobby) {
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+    }
+
+    return lobby;
   }
 
+  @ApiOperation({ summary: 'Find lobbies' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'List of lobbies',
+    type: ILobby,
+  })
   @Get()
-  findAll() {
-    return this.lobbyService.findAll();
+  async findAll() {
+    return await this.lobbyService.findAll();
   }
 
+  @ApiOperation({ summary: 'Find lobby by ID' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Lobby',
+    type: ILobby,
+  })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.lobbyService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const lobby: ILobby = await this.lobbyService.findOne(+id);
+    if (!lobby) {
+      throw new HttpException('Lobby not found', HttpStatus.NOT_FOUND);
+    }
+    return lobby;
   }
 
+  @ApiOperation({ summary: 'Update lobby' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Lobby updated',
+    type: ILobby,
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLobbyDto: UpdateLobbyDto) {
-    return this.lobbyService.update(+id, updateLobbyDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateLobbyDto: UpdateLobbyDto,
+  ) {
+    return await this.lobbyService.update(+id, updateLobbyDto);
   }
 
+  @ApiOperation({ summary: 'Delete lobby' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Lobby deleted',
+  })
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.lobbyService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.lobbyService.remove(+id);
   }
 }
