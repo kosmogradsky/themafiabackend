d3df8760c874f3cd7b7196bf4ab5bdb61649ff8e
Soslan Aldatov <me@sptm.dev>
2021-05-18 21:21:45 +0500

    add lobby service

--- mafia-server/src/lobby/lobby.service.ts
+++ mafia-server/src/lobby/lobby.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLobbyDto } from './dto/create-lobby.dto';
 import { UpdateLobbyDto } from './dto/update-lobby.dto';
+import { Lobby } from './entities/lobby.entity';
 
 @Injectable()
 export class LobbyService {
-  create(createLobbyDto: CreateLobbyDto) {}
+  constructor(
+    @InjectRepository(Lobby)
+    private lobbyRepository: Repository<Lobby>,
+  ) {}
 
-  findAll() {}
+  async create(createLobbyDto: CreateLobbyDto): Promise<Lobby> {
+    return await this.lobbyRepository.save(createLobbyDto);
+  }
 
-  findOne(id: number) {}
+  findAll(): Promise<Lobby[]> {
+    return this.lobbyRepository.find();
+  }
 
-  update(id: number, updateLobbyDto: UpdateLobbyDto) {}
+  findOne(id: number): Promise<Lobby> {
+    return this.lobbyRepository.findOne(id);
+  }
 
-  remove(id: number) {}
+  async update(id: number, updateLobbyDto: UpdateLobbyDto): Promise<Lobby> {
+    const lobby = await this.findOne(id);
+    if (!lobby) return lobby;
+
+    return await this.lobbyRepository.save(
+      Object.assign(lobby, updateLobbyDto),
+    );
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.lobbyRepository.delete(id);
+  }
 }
