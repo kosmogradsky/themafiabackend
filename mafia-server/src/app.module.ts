7127798060d92bbcf99537b0a42e0f055f64f051
Soslan Aldatov <me@sptm.dev>
2021-05-24 16:17:16 +0500

    remove unnecessary stuff & finalize

--- mafia-server/src/app.module.ts
+++ mafia-server/src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { DatabaseModule } from './core/database/database.module';
 
 import { GameModule } from './game/game.module';
-import { ApiModule } from './api/api.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { LobbyModule } from './lobby/lobby.module';
 
 @Module({
-  imports: [DatabaseModule, GameModule, ApiModule, UserModule, AuthModule, LobbyModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, GameModule, UserModule, AuthModule, LobbyModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
